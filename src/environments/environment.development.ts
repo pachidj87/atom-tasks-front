f3953765eba16dbd512d3cda08e66da9687200bc
pachidj87 <pachidj87@gmail.com>
2025-05-16 18:32:30 -0300

    Adding test api server

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 const appConfig = {
   production: false,
   // appApiUrl: 'api',
-  appApiUrl: 'http://82.98.190.78:3000/',
+  appApiUrl: 'http://82.98.190.78:3000',
   endpoints: {
     authToken: '/auth/login',
     authRegister: '/auth/register',
