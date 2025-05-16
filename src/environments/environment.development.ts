a208a54b28fda3d4718bdb0ad475cac0ddb90a36
pachidj87 <pachidj87@gmail.com>
2025-05-16 19:30:45 -0300

    Adding test api server

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 const appConfig = {
   production: false,
   // appApiUrl: 'api',
-  appApiUrl: 'http://82.98.190.78:3000',
+  appApiUrl: 'https://vercel-atom-task-api.onrender.com',
   endpoints: {
     authToken: '/auth/login',
     authRegister: '/auth/register',
